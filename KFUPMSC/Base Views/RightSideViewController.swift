88504b1e5b0d47b97cd5cbc2ff7398dbacc37bd4
Moemena AlTahhan <ammaraltahhan@ammars-macbook-pro.local>
2018-01-09 09:34:57 +0300

    Structuring app flow + Creating NewsViewController in IB

--- KFUPMSC/Base Views/RightSideViewController.swift
+++ KFUPMSC/Base Views/RightSideViewController.swift
@@ -17,27 +17,11 @@ class RightSideViewController: UIViewController {
 
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func btn(_ sender: UIButton) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         appDelegate.drawerContainer?.toggle(MMDrawerSide.right, animated: true, completion: nil)
         (mainTabbarController.selectedViewController as! UINavigationController).pushViewController("RightSideViewController")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
